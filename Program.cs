37bce734a78132a0636d8ae6ac37397cc0b0cd72
Tomoya Washio <TWashio@infragistics.com>
2024-02-15 10:21:35 +0900

    初回コミット

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using KB12938_IgbDataChart_IgbScatterSeries_MarkerColor;
+using IgniteUI.Blazor.Controls;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddIgniteUIBlazor(
+    typeof(IgbDataLegendModule),
+    typeof(IgbCategoryChartModule)
+    );
+
+await builder.Build().RunAsync();
